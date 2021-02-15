313fc93d2cfa3db0660cf2618450610898e9c410
Leonardo Santos <44562511+lcds90@users.noreply.github.com>
2021-02-15 09:08:34 -0300

    Update themoviedb.js

--- js/themoviedb.js
+++ js/themoviedb.js
@@ -11,8 +11,8 @@ var theMovieDb = {};
 
 theMovieDb.common = {
   api_key: "5c427fb5881d48a13a79a80bf0b24ef2",
-  base_uri: "http://api.themoviedb.org/3/",
-  images_uri: "http://image.tmdb.org/t/p/",
+  base_uri: "https://api.themoviedb.org/3/",
+  images_uri: "https://image.tmdb.org/t/p/",
   timeout: 5000,
   language: "pt-BR",
   generateQuery: function(options) {
